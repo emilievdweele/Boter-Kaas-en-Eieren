f78a990f679ac68ecfabb505af5f3ec9dfca7f65
emilievdweele <74767348+emilievdweele@users.noreply.github.com>
2022-10-03 14:15:58 +0000

    Werkend menu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
  
-from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot
+from bke import EvaluationAgent, MLAgent, is_winner, opponent, RandomAgent, train_and_plot, save, train, load, start
  
  
 class MyAgent(MLAgent):
@@ -14,14 +14,62 @@ class MyAgent(MLAgent):
         return reward
     
     
-random.seed(1)
+class MyRandomAgent(EvaluationAgent):
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    return random.randint(1, 500)
+
+def tegendom():
+  my_random_agent = MyRandomAgent()
+  start(player_o=my_random_agent)
+
+def trainAlleen():
+  my_agent = MyAgent()
+  
+  train(my_agent, 3000)
+  save(my_agent, 'MyAgent_3000')
+
+def tegenslim():
+  my_agent = load('MyAgent_3000')
+
+  my_agent.learning = False
+  start(player_x=my_agent)
+
+def tegenander():
+  start()
+
+def plot():
+  random.seed(1)
  
-my_agent = MyAgent(alpha=0.8, epsilon=0.2)
-random_agent = RandomAgent()
+  my_agent = MyAgent(alpha=0.8, epsilon=0.2)
+  random_agent = RandomAgent()
  
-train_and_plot(
+  train_and_plot(
     agent=my_agent,
     validation_agent=random_agent,
     iterations=50,
     trainings=100,
     validations=1000)
+
+
+print("1: Tegen een domme tegenstander spelen")
+print("2: De tegenstander trainen")
+print("3: Tegen een slimme tegenstander spelen")
+print("4: Tegen een andere persoon spelen")
+print("5: Een validatie grafiek plotten")
+
+i = input()
+
+if i == "1":
+  tegendom()
+
+if i == "2":
+  trainAlleen()
+
+if i== "3":
+  tegenslim()
+
+if i == "4":
+  tegenander()
+
+if i == "5":
+  plot()
